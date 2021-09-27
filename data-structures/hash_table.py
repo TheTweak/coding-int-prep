93a4029ee02c3f620306163624bdf9672610d002
thetweak <thetweak@cloud.upwork.com>
2021-09-27 11:00:29 +0300

    add delete operation to hash map

--- data-structures/hash_table.py
+++ data-structures/hash_table.py
@@ -1,6 +1,8 @@
 class HashTable:
     __items = [[] for _ in range(32)]
 
+    # TODO: rewrite using Node class
+
     def __get_index(self, key):
         return hash(key) % len(self.__items)
 
@@ -20,12 +22,21 @@ class HashTable:
                 return v
         return None
 
+    def delete(self, key):
+        idx = self.__get_index(key)
+        for i, (k, v) in enumerate(self.__items[idx]):
+            if k == key:
+                self.__items[idx].remove((k, v))
+
 
 if __name__ == '__main__':
     table = HashTable()
     table.put('abc', 42)
+    table.delete('abc')
     table.put('abc', 31337)
     table.put('d', 1)
     print(f'abc={table.get("abc")}')
     print(f'd={table.get("d")}')
     print(f'qwe={table.get("qwe")}')
+    table.delete('abc')
+    print(f'abc={table.get("abc")}')
