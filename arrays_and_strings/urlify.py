c6cbf2607d1d9933a57811073fbacde57b7135c3
thetweak <thetweak@cloud.upwork.com>
2021-09-28 10:53:33 +0300

    URLify v2

--- arrays_and_strings/urlify.py
+++ arrays_and_strings/urlify.py
@@ -11,13 +11,31 @@ def count_tail(s: list) -> int:
     return cnt
 
 
+def count_ws(s: list) -> int:
+    cnt = 0
+    for c in s:
+        if c == ' ':
+            cnt += 1
+    return cnt
+
+
 if __name__ == '__main__':
     in_str = list(next(fileinput.input()))[:-1]
-    print(in_str)
-    ws_cnt = count_tail(in_str)
-    print(ws_cnt)
-    len_wo_ws = len(in_str) - ws_cnt
-    len_full = len(in_str) - 1
+    len_wo_ws = len(in_str)
+
+    # count whitespaces
+    ws_cnt = count_ws(in_str)
+
+    # appen empty chars to acquire enough space for
+    # %20
+    for i in range(ws_cnt):
+        in_str.append(' ')
+        in_str.append(' ')
+    len_full = len(in_str)-1
+
+    # moving backwards through original string
+    # substitute ws with %20
+    # move everything else to fill gaps in new string
     for i in range(len_wo_ws-1, 0, -1):
         c = in_str[i]
         if c == ' ':
