a8e449c6ce6f201cec77622d67fd29c48bd66cbd
thetweak <thetweak@cloud.upwork.com>
2021-10-07 23:05:17 +0300

    improve lists sum solution by reusing one of the lists to store result

--- linked_lists/sum_lists.py
+++ linked_lists/sum_lists.py
@@ -20,8 +20,10 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        self.size = 0
 
     def add(self, value: int) -> Node:
+        self.size += 1
         if not self.head:
             self.head = Node()
             self.head.value = value
@@ -55,7 +57,13 @@ brute force:
     - reverse the lists first
     - reverse the result
 
-    improvement: use one of the linked lists to store the result
+    improvement: use longest of the linked lists to store the result
+
+    1->2->3
+
+    1->4
+    
+    2->6->3
 
 '''
 
@@ -76,39 +84,56 @@ def reverse(l: LinkedList) -> None:
 def sum_lists(a: LinkedList, b: LinkedList) -> LinkedList:
     reverse(a)
     reverse(b)
-    result = LinkedList()
     overflow = False
+    a_is_longer = True
+    if b.size > a.size:
+        a_is_longer = False
     n = a.head
     m = b.head
     while n or m:
         s = 0
         if n:
             s += n.value
-            n = n.next
         if m:
             s += m.value
-            m = m.next
         if overflow:
             s += 1
             overflow = False
         if s // 10:
             overflow = True
-            result.add(s%10)
+            if a_is_longer:
+                n.value = s%10
+            else:
+                m.value = s%10
         else:
-            result.add(s)
+            if a_is_longer:
+                n.value = s
+            else:
+                m.value = s
+        if n:
+            n = n.next
+        if m:
+            m = m.next
 
     if overflow:
-        result.add(1)
+        if a_is_longer:
+            a.add(1)
+        else:
+            b.add(1)
 
-    reverse(result)
-    return result
+    if a_is_longer:
+        reverse(a)
+        return a
+    else:
+        reverse(b)
+        return b
 
 
 if __name__ == '__main__':
     a = LinkedList()
-    a.add(9)
-    a.add(9)
+    a.add(1)
     a.add(3)
+    a.add(2)
     '''
     reverse(a)
     n = a.head
@@ -118,7 +143,10 @@ if __name__ == '__main__':
     '''
 
     b = LinkedList()
-    b.add(7)
+    b.add(2)
+    b.add(2)
+    b.add(1)
+    b.add(1)
 
     s = sum_lists(a, b)
 
