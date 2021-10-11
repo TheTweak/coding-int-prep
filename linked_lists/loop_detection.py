3c1f46ddfba7c2d516df6fe3c4b36c2104ee9cb4
thetweak <thetweak@cloud.upwork.com>
2021-10-11 15:20:55 +0300

    loop detection

---
+++ linked_lists/loop_detection.py
@@ -0,0 +1,79 @@
+'''
+Check if a linked list contains a loop. Return the beginning of the lope node.
+'''
+
+class Node:
+    
+    def __init__(self):
+        self.next = None
+        self.value = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def add(self, value: str) -> Node:
+        self.size += 1
+        if not self.head:
+            self.head = Node()
+            self.head.value = value
+            return self.head
+        else:
+            tail = self.head
+            while tail.next:
+                tail = tail.next
+            tail.next = Node()
+            tail.next.value = value
+            return tail.next
+
+
+'''
+brute force:
+    keep a dict/set of visited nodes, if node is seen again, then there is a loop
+
+    time: O(N)=N
+    space: O(N)=N
+
+---
+space improvement: if list size is known, go over the list and keep a counter
+if counter > list size, then there is a loop
+
+space: O(N)=1
+'''
+def check_loop(ll: LinkedList) -> Node:
+    visited = set()
+    n = ll.head
+    while n:
+        if id(n) in visited:
+            return n
+        visited.add(id(n))
+        n = n.next
+    return None
+
+
+def check_loop_size(ll: LinkedList) -> Node:
+    i = ll.size
+    n = ll.head
+    while n:
+        if i < 0:
+            return n.next
+        n = n.next
+        i -= 1
+    return None
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.add('a')
+    ll.add('b')
+    c = ll.add('c')
+    d = ll.add('d')
+    d.next = c
+
+    loop = check_loop_size(ll)
+    if loop:
+        print(f'loop detected: {loop.value}')
+
