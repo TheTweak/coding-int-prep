1683c709b89894aca48fa762e3253e407360211f
thetweak <thetweak@cloud.upwork.com>
2021-10-11 17:40:06 +0300

    improve kth to last in a linked list using slow and fast (k-ahead) pointers

--- linked_lists/kth_to_last.py
+++ linked_lists/kth_to_last.py
@@ -42,11 +42,55 @@ def get_k_to_last(ll: LinkedList, k: int) -> None:
     return kq.popleft()
 
 
+'''
+Maintain two pointers k length apart.
+time O(n)=n
+space O(n)=1
+'''
+def get_k_to_last_point(ll: LinkedList, k: int) -> None:
+    p1 = ll.head
+    while p1 and k > 0:
+        p1 = p1.next
+        k -= 1
+
+    if k != 0:
+        return None
+
+    n = ll.head
+    m = p1
+    while n and m:
+        n = n.next
+        m = m.next
+
+    return n
+
+
+'''
+a -> b -> c -> d, k=1
+
+d, 1, 0
+c, 1, 1
+b, 1, 2
+a, 1, 3
+
+'''
+
+def get_k_to_last_recursive(x: Node, k: int, counter: list) -> Node:
+    if not x:
+        return None
+
+    result = get_k_to_last_recursive(x.next, k, counter)
+    counter[0] += 1
+    if counter[0] == k:
+        return x
+    return result
+
 if __name__ == '__main__':
     ll = LinkedList()
     ll.add(1)
     ll.add(2)
     ll.add(4)
     ll.add(7)
-    print(get_k_to_last(ll, 4).value)
+    counter = [0]
+    print(get_k_to_last_recursive(ll.head, 2, counter).value)
 
