061ede19bf9de3fbf67d85e24a6c3cb886bab759
TheTweak <tweakon@gmail.com>
2021-12-14 15:18:41 +0300

    dynamic programming: powerset solution using combinatorics

--- dynamic_programming/power_set.py
+++ dynamic_programming/power_set.py
@@ -42,8 +42,33 @@ def power_set2(orig: set) -> list[set]:
     return result[:-1]
 
 
+def power_set_combin(orig: set) -> list[set]:
+    '''
+    If we represent each possible subset of orig set as a binary
+    number, where 1 - means element of orig set goes into subset,
+    and 0 - does not go into the subset, then we can just iterate
+    from 0 to integer with bits of size=size(orig) and all 1s,
+    convert each i to binary representation, and create a subset using it.
+    '''
+    orig = list(orig)
+    result = []
+    n = 1 << len(orig)
+    for i in range(n):
+        subset = set()
+        bin_repr = i
+        j = 0
+        while bin_repr:
+            if bin_repr & 1:
+                subset.add(orig[j])
+            bin_repr >>= 1
+            j += 1
+        result.append(subset)
+    return result
+
+
 if __name__ == '__main__':
-    orig = set([1, 2, 3, 4, 5, 6])
-    powerset = power_set2(orig)
+    orig = set([1, 2, 3, 4])
+    powerset = power_set_combin(orig)
+    print(len(powerset))
     for s in powerset:
         print(s)
