46d3021a46c8a55289b3a8ab9b9196e2d8415e18
TheTweak <tweakon@gmail.com>
2021-12-22 16:19:11 +0300

    dynamic programming: coins brute force and DP solution for case with repetitions

---
+++ dynamic_programming/coins.py
@@ -0,0 +1,89 @@
+'''
+Given an inf. number of coins: 25, 10, 5, 1 calculate a number of ways to represent N cents.
+
+1) Greedy algorithm
+
+calc_ways(coins, ways, sum)
+- if sum == N, ways++ and return
+- pick next coin
+- if sum + coin <= N, sum += coin
+-   calc_ways(coins, ways, sum)
+    sum -= coin
+- else pick next coin
+
+2) Dynamic programming
+
+with repetitions:
+
+count(N) = count(N-1) + count(1)
+
+count(0) = 1
+count(1) = 1
+count(2) = count(1) = 1
+count(3) = count(2) = 1
+count(4) = count(3) = 1
+count(5) = count(4) + count(0) = 2
+count(6) = count(5) + count(1) = 2 + 1 = 3
+count(10) =  
+
+wo repetitions:
+
+
+
+
+'''
+
+
+class BruteForceSolution:
+    def __init__(self, n):
+        self.n = n
+        self.coins = [25, 10, 5, 1]
+        self.visited = set()
+
+    def __calc_ways__(self, summ: int, coin_set: list[int]) -> None:
+        if summ == self.n:
+            coin_set = sorted(coin_set)
+            cs_key = ''.join([str(c) for c in coin_set])
+            if not cs_key in self.visited:
+                print(coin_set)
+            self.visited.add(cs_key)
+            return
+
+        for c in self.coins:
+            if summ + c <= self.n:
+                coin_set.append(c)
+                self.__calc_ways__(summ + c, coin_set)
+                coin_set.pop()
+
+    def solve(self):
+        self.__calc_ways__(0, [])
+        return len(self.visited)
+
+
+class DPSolution:
+    def __init__(self, n):
+        self.n = n
+        self.dp = [0 for _ in range(n+1)]
+        self.dp[0] = 1
+        self.coins = [25, 10, 5, 1]
+
+    def solve(self):
+        for i in range(1, self.n + 1):
+            for c in self.coins:
+                if i - c >= 0:
+                    self.dp[i] += self.dp[i-c]
+
+        return self.dp[self.n]
+
+
+if __name__ == '__main__':
+    print(BruteForceSolution(16).solve())
+    assert BruteForceSolution(1).solve() == 1
+
+    print(DPSolution(6).solve())
+
+
+
+
+
+
