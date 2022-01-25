69b8b10f3b365a1dd646a341fed7d68cbd6ac7b9
thetweak <thetweak@cloud.upwork.com>
2022-01-25 16:02:09 +0300

    search and sort: search in sparse array

--- search_and_sort/sparse_search.py
+++ search_and_sort/sparse_search.py
@@ -7,21 +7,62 @@ output: 4
 '''
 
 
-def skip_empty(n: list[str], start: int, end: int) -> int:
+def skip_empty(n: list[str], l: int, h: int) -> int:
 	'''
-	Finds first non empty element on interval [start, end], going from end->lower
+	Finds first non empty element on interval [l, h], going from h->l
 	and returns its index. Returns -1 if no non empty elemnts found.
 	'''
-	if start > end:
-		return -1
-
-
-
-def search(n: list[str], el: str, l: int, h: int) -> int:
 	if l > h:
 		return -1
 
 	m = (l + h) // 2
+	if n[m] != '':
+		return m
+	else:
+		return skip_empty(n, l, m - 1)
+
+
+def get_new_middle(n: list[str], l: int, h: int) -> int:
+	m = (l + h) // 2
+	m_ = skip_empty(n, l, m - 1) # try left half
+	if m_ != -1:
+		return m_
+	return skip_empty(n, m + 1, h) # try right half
+
+
+def search(n: list[str], el: str, l: int, h: int) -> int:	
+	if l >= h:
+		return -1
+
+	m = (l + h) // 2
+	if n[m] == '':
+		m = get_new_middle(n, l, h)
+		if m == -1:
+			return -1
+	
 	x = n[m]
-	if x == '':
-		m = skip_empty(n, l, m - 1)
+	if x == el:
+		return m
+	elif x > el:
+		return search(n, el, l, m - 1)
+	else:
+		return search(n, el, m + 1, h)
+
+
+if __name__ == '__main__':
+	n = ["at", "", "", "", "ball", "", "", "", "cat"]
+	i = search(n, 'ball', 0, len(n))
+	print(f'i={i}')
+	assert i == 4
+
+	i = search(n, 'at', 0, len(n))
+	print(f'i={i}')
+	assert i == 0
+
+	i = search(n, 'dog', 0, len(n))
+	print(f'i={i}')
+	assert i == -1
+
+	i = search(n, 'cat', 0, len(n))
+	print(f'i={i}')
+	assert i == 8
